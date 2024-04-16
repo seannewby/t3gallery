1bc8d7899226fc703c0eb28170ae57442a7486bd
sean <sean@seannewby.com>
2024-04-15 19:39:56 -0700

    more upload things

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -9,7 +9,7 @@ const f = createUploadthing();
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 10 } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
@@ -27,7 +27,9 @@ export const ourFileRouter = {
 
       console.log("file url", file.url);
 
-      await db.insert(images).values({ name: file.name, url: file.url });
+      await db
+        .insert(images)
+        .values({ name: file.name, url: file.url, userId: metadata.userId });
 
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
       return { uploadedBy: metadata.userId };
